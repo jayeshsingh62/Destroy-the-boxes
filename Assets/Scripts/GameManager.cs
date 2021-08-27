e4a852aff64810a8cce108e15666cae63a18ceb3
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-27 19:00:00 +0530

    call the timer method

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
