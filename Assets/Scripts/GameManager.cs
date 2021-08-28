735ae8e3fc287f8936f832b7b50c3d1d45bace97
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-28 19:58:26 +0530

    add 5 to the score everytime a target spawns

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -30,6 +30,7 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            UpdateScore(5);
         }
     }
 
