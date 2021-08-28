4f417745e3fadb241f4bacaeaed61fc5f03f42e9
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-28 21:31:59 +0530

    update the score according to points assigned to a target

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -33,7 +33,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
