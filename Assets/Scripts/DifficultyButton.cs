bceb6df365dc94d7531ad279882071f3fa0064c9
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-31 19:38:22 +0530

    get the reference to the game manager script

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -13,6 +13,7 @@ public class DifficultyButton : MonoBehaviour
     {
         button = GetComponent<Button>();
         button.onClick.AddListener(SetDifficulty);
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
