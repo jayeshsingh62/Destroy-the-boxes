7b3e452ceb6069da25c15142f1d40fc00f0f8c5c
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-31 19:26:50 +0530

    get the reference to the button component

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -10,7 +10,7 @@ public class DifficultyButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        button = GetComponent<Button>();
     }
 
     // Update is called once per frame
