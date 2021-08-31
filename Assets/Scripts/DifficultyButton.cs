ab1a6fc80a55700d92484d366c5bdb37eb2f9d31
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-31 19:37:33 +0530

    added a new game manager variable

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class DifficultyButton : MonoBehaviour
 {
     private Button button;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
